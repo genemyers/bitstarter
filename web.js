0a7dc49a35c2936c8826105a63f55d1c61721ee2
genemyers <amazon@datumlogic.com>
2013-08-04 17:06:43 +0000

    updated package.json to include stripe, and uncommented web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-//var api_key = 'sk_test_AdiVCMgrDSynAgkxnpC8Tjqw';  // test private_key
-//var stripe = require('stripe')(api_key);
+var api_key = 'sk_test_AdiVCMgrDSynAgkxnpC8Tjqw';  // test private_key
+var stripe = require('stripe')(api_key);
 
 var page = fs.readFileSync('index.html','utf8');
 var app = express.createServer(express.logger());
 //must be here 
-//app.use(express.bodyParser());
+app.use(express.bodyParser());
 
 
 app.get('/', function(request, response) {
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
 });
 
 
-/*
+
 app.post('/', function(request, response) {  
   var token = request.param('stripeToken',null); // second parameter is default
   //as an alt to request.param, you can ref the params directly like this: request.body.stripeToken
@@ -35,7 +35,7 @@ app.post('/', function(request, response) {
      }
    );
 });
-*/
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
